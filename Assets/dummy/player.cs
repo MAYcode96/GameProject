814bf0a2268d3c99081e76c60975efd345f37643
MAYcode96 <maycode96@gmail.com>
2026-04-21 22:29:55 +0700

    progress bossFight:karakter move grid

---
+++ Assets/dummy/player.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class PlayerDrag2D : MonoBehaviour
+{
+    private Vector3 offset;
+    private Vector3 startPosition;
+    private Transform startTile;
+
+    private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void OnMouseDown()
+    {
+        Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+
+        offset = transform.position - mousePos;
+
+        startPosition = transform.position;
+        startTile = transform.parent;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+
+        transform.position = mousePos + offset;
+    }
+
+    void OnMouseUp()
+    {
+        Collider2D hit = Physics2D.OverlapPoint(transform.position);
+
+        if (hit != null && hit.CompareTag("tile"))
+        {
+            Transform targetTile = hit.transform;
+
+            if (targetTile.childCount == 0)
+            {
+                transform.position = targetTile.position;
+                transform.parent = targetTile;
+                return;
+            }
+        }
+
+        transform.position = startPosition;
+        transform.parent = startTile;
+    }
+}
